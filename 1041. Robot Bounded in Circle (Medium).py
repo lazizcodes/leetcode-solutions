66264c0ab9db0400af8e0c56550fd00f306e002c
lazizcodes <59307120+lazizcodes@users.noreply.github.com>
2020-11-06 22:56:14 +0500

    Update 1041. Robot Bounded in Circle (Medium).py

--- 1041. Robot Bounded in Circle (Medium).py
+++ 1041. Robot Bounded in Circle (Medium).py
@@ -9,19 +9,13 @@ class Solution:
         while True:
             i += 1
             for l in instructions:
-                if l == 'R': 
-                    d += 1
-                if l == 'L': 
-                    d -= 1
+                if l == 'R': d += 1
+                if l == 'L': d -= 1
                 if l == 'G':   
-                    if d % 4 == 0: 
-                        y += 1
-                    if d % 4 == 1: 
-                        x += 1
-                    if d % 4 == 2: 
-                        y -= 1
-                    if d % 4 == 3: 
-                        x -= 1
+                    if d % 4 == 0: y += 1
+                    if d % 4 == 1: x += 1
+                    if d % 4 == 2: y -= 1
+                    if d % 4 == 3: x -= 1
             if i >= n and (x == 0 and y == 0):
                 return True
             if i >= n and d == 0:
